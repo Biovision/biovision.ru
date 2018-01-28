238c505c203754d11e229733b7f105694cb0f6ae
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-01-28 23:29:08 +0300

    Updated bundle and added metrika counter

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :branch, 'master'
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-set :shared_dirs, fetch(:shared_dirs, []).push('public/uploads', 'tmp', 'log')
+set :shared_dirs, fetch(:shared_dirs, []).push('public/uploads', 'public/git', 'tmp', 'log')
 set :shared_files, fetch(:shared_files, []).push('.env')
 
 # This task is the environment that is loaded for all remote run commands, such as
